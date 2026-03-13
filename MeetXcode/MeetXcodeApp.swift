96ac9b25a3bfcecb96ba6d0776e3850282ff0377
Jose <jeduramos7001@gmail.com>
2026-03-13 02:10:23 -0700

    Initial commit

---
+++ MeetXcode/MeetXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetXcodeApp.swift
+//  MeetXcode
+//
+//  Created by Jose Ramos on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
